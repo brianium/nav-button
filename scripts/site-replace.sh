1c6848e1ad3103983d28d104213b8233ea80993a
Brian Scaturro <scaturrob@gmail.com>
2018-01-06 11:24:12 -0500

    Includes site deploy script

---
+++ scripts/site-replace.sh
@@ -0,0 +1,8 @@
+# strip dev prefixed paths to work with gh pages
+sed -i "" 's/\/dev\///g' site/index.html
+
+# replaces site placeholder with appropriate contents
+sed -i "" 's/<!-- build:site -->/<link rel="stylesheet" href="nav-button.css">/g' site/index.html
+
+# strip leading slash from dev bundle path
+sed -i "" 's/\/nav-button/nav-button/g' site/index.html
