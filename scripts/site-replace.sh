a77442e9acf368a276a6c06d7bff47c2c5c68c98
Brian Scaturro <scaturrob@gmail.com>
2018-01-06 16:04:01 -0500

    Update site build

--- scripts/site-replace.sh
+++ scripts/site-replace.sh
@@ -5,5 +5,5 @@ sed -i "" 's/\/dev\///g' site/index.html
 sed -i "" 's/<!-- build:site -->/<link rel="stylesheet" href="nav-button.css">/g' site/index.html
 
 # strip leading slash from dev bundle path
-sed -i "" 's/\/nav-button/nav-button/g' site/index.html
-sed -i "" 's/\/nav-button-react/nav-button-react/g' site/index.html
+sed -i "" 's/\/nav-button.js/nav-button.js/g' site/index.html
+sed -i "" 's/\/nav-button-react.js/nav-button-react.js/g' site/index.html
